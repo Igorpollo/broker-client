066f4cc1daecf8e7190ae5c6846ad2376a119663
Igor Martins <igorpollo@gmail.com>
2017-11-08 21:36:22 -0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header-app></header-app>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Layout/Header'
+
+export default {
+  name: 'app',
+  components: {
+    'header-app': Header
+  }
+}
+</script>
+
+<style>
+.el-menu-demo {
+  margin-bottom: 60px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
