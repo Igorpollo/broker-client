48b9a58b7fe82bf7e2febf61841560a2bee07216
Igor Martins <igorpollo@gmail.com>
2017-11-09 16:44:06 -0200

    mudanca

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="hello">
     <el-row>
-      <el-col :span="12"><div class="grid-content bg-purple">teste1</div></el-col>
+      <el-col :span="12">
+        <user-detail></user-detail>
+      </el-col>
       <el-col :span="12"><div class="grid-content bg-purple-light">teste 2</div></el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import UserDetail from '@/components/UserDetail'
+
 export default {
   name: 'hello',
+  components: {
+    'user-detail': UserDetail
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Nome: {{name}}</h3>
+    <ul id="example-1">
+      <li v-for="item in messages">
+        {{ item.text }}
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+import {HTTP} from '../http-common'
+
+export default {
+  name: 'UserDetail',
+  data () {
+    return {
+      name: 'Igor',
+      messages: []
+    }
+  },
+  created () {
+    HTTP.get('messages')
+    .then(response => {
+      this.messages = response.data
+    })
+    .catch(e => {
+    })
+  }
+}
+</script>
